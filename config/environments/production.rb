7535153a0261ac0f383c782170f7a78faee92e5e
ShintaroKoizumi <shintakoizu@gmail.com>
2017-10-08 01:40:41 +0900

    git ignore 変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,15 +89,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'cryptic-reaches-99504' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
- {
-  user_name: ENV['SENDGRID_USERNAME'], ##変更
-  password: ENV['SENDGRID_PASSWORD'], ##変更
-  domain: "example.com",
+  {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: "heroku.com",
   address: "smtp.SendGrid.net",
   port: 587,
   authentication: :plain,
   enable_starttls_auto: true
- }
+  }
 end
